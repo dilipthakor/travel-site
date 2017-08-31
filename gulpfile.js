2b91ce0bc885d0363fd5f04302f5b5cd6300741c
dilipthakor <thakordilip8@gmail.com>
2017-09-01 05:10:25 +0530

    gulp.src pipe packeges postcss nested postcss-simple-var

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,9 @@
-var gulp = require('gulp'), watch = require('gulp-watch');
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested');
 
 
 gulp.task('default',function(){
@@ -10,7 +15,9 @@ gulp.task('html',function(){
 	});
 	
 gulp.task('styles',function(){
-	return gulp.src('./app/assets/styles/styles.css').pipe(gulp.dest('./app/temp/styles'));
+	return gulp.src('./app/assets/styles/styles.css')
+	  .pipe(postcss([cssvars, nested, autoprefixer]))
+	  .pipe(gulp.dest('./app/temp/styles'));
 	});
 
 gulp.task('watch',function(){
